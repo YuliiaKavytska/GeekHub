9dec70e0462ac00aa5a61e7a185df7f9d95cd7ce
Юлия Кавицкая <61753769+YuliiaKavytska@users.noreply.github.com>
2020-10-26 00:19:15 +0200

    edit hw

--- functions/index.js
+++ functions/index.js
@@ -11,11 +11,11 @@ document.querySelectorAll('[name], #formula').forEach(function(input){
 
 		try{
 			var calc = new Function('a1, b1, a2, b2', 'return ' + formula.value + ';' );
-			var color = new Function('a1, b1, a2, b2, result, condition', 'if(' + condition.value + '){ result.style.background = "#b6d7a8"; }else{ result.style.background = "#fff"; }');
-			if(formula.value){
+			var color = new Function('a1, b1, a2, b2, result, condition', 'if(' + condition.value + '){ result.style.backgroundColor = "#b6d7a8"; }else{ result.style.backgroundColor = "#fff"; }');
+			color(a1.value, b1.value, a2.value, b2.value, result, condition);
+			if(formula.value != ""){
 				result.value = calc(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
 			}
-			color(a1.value, b1.value, a2.value, b2.value, result, condition);
 		}catch(error){
 			result.value = "#ERROR";
 			console.error(error.message);
