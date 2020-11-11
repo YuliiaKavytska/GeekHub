de00b5185cbaf513ae40de3eca1dd981cbdc7b13
Юлия Кавицкая <61753769+YuliiaKavytska@users.noreply.github.com>
2020-11-11 22:05:57 +0200

    dote additional homework

--- dom/index.js
+++ dom/index.js
@@ -1,16 +1,18 @@
+table = jQuery('table');
+var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
 jQuery('input').on('paste', function (e) {
 	e.preventDefault();
 
 	var text = e.originalEvent.clipboardData.getData('text/plain');
 	var input = e.currentTarget;
 
-	var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
+	
 	var array = text.split("\n").map(function (e) {
 		return e.split(";").map(String);
 	});
 	var row = input.parentNode.parentNode.rowIndex;
 	var column = input.offsetParent.cellIndex;
-	table = jQuery('table');
+	
 	console.log(array);
 	console.log("row " + row);
 	console.log("column " + column);
@@ -45,12 +47,13 @@ jQuery('input').on('paste', function (e) {
 		}
 	}
 
-	console.log(table[0].children[1].childElementCount);
 	for (let i = 0; i < table[0].children[1].childElementCount - (row - 1); i++) {
 		for (let j = 0; j < array[i].length; j++) {
 			table[0].children[1].children[row + i - 1].children[j + column].children[0].value = array[i][j];
 		}
 	}
+	addListRowAll();
+	addListToAll();
 });
 // Євпак Віктор Миколайович;ФОП;1985;2;3;55
 // Бондаренко Анатолій Васильович;міський голова;1974;2;3;55
@@ -62,40 +65,125 @@ jQuery('input').on('paste', function (e) {
 // Мойсієнко Василь Миколайович;перший проректор;1965;2;3;55
 
 var currentColumn;
+var indexColumn;
 
-jQuery('thead th').on('contextmenu', function (e) {
-	e.preventDefault();
-
-	currentColumn = e.currentTarget;
-
-	var menu = jQuery('#column-menu');
-
-	menu.addClass('d-block');
-
-	menu.css({
-		left: e.clientX,
-		top: e.clientY
+var menu = jQuery('#column-menu');
+function addListToAll(){
+	jQuery('thead th').on('contextmenu', function (e) {
+		e.preventDefault();
+		currentColumn = e.currentTarget;
+		indexColumn = currentColumn.cellIndex;
+		if(indexColumn != 0){
+			menu.addClass('d-block');
+			menu.css({
+				left: e.clientX,
+				top: e.clientY
+			});
+		}else{
+			menu.removeClass('d-block');
+		}
 	});
-});
+}
+addListToAll();
 
 jQuery('#column-menu [data-action]').on('click', function (e) {
 	e.preventDefault();
 
 	var action = e.currentTarget.getAttribute('data-action');
-
+	
 	switch (action) {
 		case 'add-left':
-
+			currentColumn.insertAdjacentHTML("beforeBegin", '<th></th>');
+			for(let i = 0; i < table[0].children[1].childElementCount; i++){
+				table[0].children[1].children[i].children[indexColumn].insertAdjacentHTML("beforeBegin", '<td><input type="text" name="" value=""/></td>');
+			}
 			break;
 
 		case 'add-right':
-
+			currentColumn.insertAdjacentHTML("afterEnd", '<th></th>');
+			for(let i = 0; i < table[0].children[1].childElementCount; i++){
+				table[0].children[1].children[i].children[indexColumn].insertAdjacentHTML("afterEnd", '<td><input type="text" name="" value=""/></td>');
+			}
 			break;
 
 		case 'remove':
-
+			currentColumn.remove();
+			for(let i = 0; i < table[0].children[1].childElementCount; i++){
+				table[0].children[1].children[i].children[indexColumn].remove();
+			}
 			break;
 	}
+	
+	addListToAll();
+	reriteNames();
+	jQuery('#column-menu').removeClass('d-block');
+});
 
+function reriteNames() {
+	var quantitlyColumn = table[0].children[0].children[0].childElementCount;
+	for(let i = 1; i < quantitlyColumn; i++){
+		table[0].children[0].children[0].children[i].innerText = alphabet[i - 1].toLocaleUpperCase();
+	}
+
+	var quantitlyRows = table[0].children[1].childElementCount;
+	
+	for(let i = 0; i < quantitlyRows; i++){
+		var quantitlyItems = table[0].children[1].children[i].childElementCount;
+		for(let j = 0; j < quantitlyItems - 1; j++){
+			table[0].children[1].children[i].children[j + 1].name = alphabet[j] + (i + 1);
+		}
+	}
+	for(let i = 0; i < quantitlyRows; i++){
+		table[0].children[1].children[i].children[0].innerText = i + 1;
+	}
+}
+
+var rowMenu = jQuery('#row-menu');
+var currentRow;
+var indexRow;
+function addListRowAll() {
+	jQuery('tbody tr th').on('contextmenu', function (e) {
+		e.preventDefault();
+		currentRow = e.currentTarget;
+		indexRow = Number(currentRow.innerText) - 1;
+		rowMenu.addClass('d-block');
+		rowMenu.css({
+				left: e.clientX,
+				top: e.clientY
+			});
+	
+		console.dir(currentRow);
+	});
+}
+addListRowAll();
+
+jQuery('#row-menu [data-action]').on('click', function (e) {
+	e.preventDefault();
+	rowMenu.removeClass('d-block');
+	var action = e.currentTarget.getAttribute('data-action');
+
+	switch (action) {
+		case 'add-above':
+			currentRow.parentNode.insertAdjacentHTML("beforeBegin", '<tr></tr>');
+			currentRow.parentNode.previousElementSibling.innerHTML += "<th>" + indexRow + "</th>";
+			for(let i = 0; i < table[0].children[1].children[indexRow + 1].childElementCount - 1; i++){
+				table[0].children[1].children[indexRow].innerHTML += '<td><input type="text" name="' + alphabet[i] + (indexRow + 1) + '" value=""/></td>';
+			}
+			break;
+
+		case 'add-under':
+			currentRow.parentNode.insertAdjacentHTML("afterEnd", '<tr></tr>');
+			currentRow.parentNode.nextElementSibling.innerHTML += "<th>" + indexRow + "</th>";
+			for(let i = 0; i < table[0].children[1].children[indexRow].childElementCount - 1; i++){
+				table[0].children[1].children[indexRow + 1].innerHTML += '<td><input type="text" name="' + alphabet[i] + (indexRow + 2) + '" value=""/></td>';
+			}
+			break;
+
+		case 'remove':
+			currentRow.parentNode.remove();
+			break;
+	}
+	reriteNames();
+	addListRowAll();
 	jQuery('#column-menu').removeClass('d-block');
 });
