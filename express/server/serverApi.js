06c530647cf28c89554e302cc05551c05c294f48
Yuliia Kavytska-ubuntu <julia.k106.l7@gmail.com>
2021-02-15 19:58:44 +0200

    fix catch write file

--- express/server/serverApi.js
+++ express/server/serverApi.js
@@ -4,30 +4,30 @@ const {resolve} = require('path');
 const fs = require('fs/promises');
 
 router.get("/all", (req, res) => {
-    fs.readFile(resolve(__dirname, 'todo.json')).then(readData => {
+    readF().then(readData => {
         const todoObj = JSON.parse(readData);
         res.json({list: todoObj.list});
     }).catch(err => {
-        returnError(res, err, 'Can`t read the file');
+        returnError(res, err);
     })
 });
 
 router.get('/completeAll', (req, res) => {
-    fs.readFile(resolve(__dirname, 'todo.json')).then(readData => {
+    readF().then(readData => {
         let todoObj = JSON.parse(readData);
 
         todoObj['list'] = todoObj.list.some(elem => elem.status === 'active')
             ? todoObj.list.map(element => ({...element, status: "completed"}))
             : todoObj.list.map(element => ({...element, status: "active"}));
 
-        writeFile(todoObj).then(() => res.end());
+        writeFile(todoObj).then(() => res.end()).catch(err => returnError(res, err));
     }).catch(err => {
         returnError(res, err);
     });
 });
 
 router.put('/changeTodo', (req, res) => {
-    fs.readFile(resolve(__dirname, 'todo.json')).then(readData => {
+    readF().then(readData => {
         let todoObj = JSON.parse(readData);
         const data = req.body;
 
@@ -35,53 +35,53 @@ router.put('/changeTodo', (req, res) => {
             ? {...element, status: element.status === "completed" ? "active" : 'completed'}
             : element);
 
-        writeFile(todoObj).then(() => res.end());
+        writeFile(todoObj).then(() => res.end()).catch(err => returnError(res, err));
     }).catch(err => {
         returnError(res, err);
     });
 });
 
 router.delete('/changeTodo', (req, res) => {
-    fs.readFile(resolve(__dirname, 'todo.json')).then(readData => {
+    readF().then(readData => {
         let todoObj = JSON.parse(readData);
         const data = req.body;
 
         todoObj['list'] = todoObj.list.filter(el => el.id !== data.id);
 
-        writeFile(todoObj).then(() => res.end());
+        writeFile(todoObj).then(() => res.end()).catch(err => returnError(res, err));
     }).catch(err => {
         returnError(res, err);
     });
 })
 
 router.post('/newTodo', (req, res) => {
-    fs.readFile(resolve(__dirname, 'todo.json')).then(readData => {
+    readF().then(readData => {
         let todoObj = JSON.parse(readData);
         const data = req.body;
 
         const id = todoObj.list.length > 0 ? todoObj.list[todoObj.list.length - 1].id + 1 : 1
         todoObj.list.push({id, task: data.task, status: "active", editing: false});
 
-        writeFile(todoObj).then(() => res.end());
+        writeFile(todoObj).then(() => res.end()).catch(err => returnError(res, err));
     }).catch(err => {
         returnError(res, err);
     });
 })
 
 router.delete('/deleteCompleted', (req, res) => {
-    fs.readFile(resolve(__dirname, 'todo.json')).then(readData => {
+    readF().then(readData => {
         let todoObj = JSON.parse(readData);
 
         todoObj['list'] = todoObj.list.filter(e => e.status !== 'completed');
 
-        writeFile(todoObj).then(() => res.end());
+        writeFile(todoObj).then(() => res.end()).catch(err => returnError(res, err));
     }).catch(err => {
         returnError(res, err);
     });
 })
 
 router.post('/changeTodo', (req, res) => {
-    fs.readFile(resolve(__dirname, 'todo.json')).then(readData => {
+    readF().then(readData => {
         let todoObj = JSON.parse(readData);
         const data = req.body;
 
@@ -90,12 +90,13 @@ router.post('/changeTodo', (req, res) => {
             task: data.task
         } : element)
 
-        writeFile(todoObj).then(() => res.end());
-    }).catch(err => {
-        returnError(res, err);
-    });
+        writeFile(todoObj).then(() => res.end()).catch(err => returnError(res, err));
+    }).catch(err => returnError(res, err));
 })
 
+const readF = () => {
+    return fs.readFile(resolve(__dirname, 'todo.json'));
+}
 
 const writeFile = (todoObj) => {
     todoObj = JSON.stringify(todoObj);
@@ -103,7 +104,7 @@ const writeFile = (todoObj) => {
 }
 
 function returnError(res, err) {
-    console.log(err);
+    console.log('Caught error: ' + err);
     if (err.code === 'ENOENT') {
         res.status(500).json({message: 'Can`t read the file'});
     } else {
